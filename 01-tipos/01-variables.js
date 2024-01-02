fadf20d7589516d03b475ce17e1d52cf2d445d74
Luca <almadaluca1104@gmail.com>
2024-01-02 16:54:59 -0300

    Curso de video 3hs

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,10 @@
+UpperCamelCase, camelCase, snake_case /* Nombres de las 3 convenciones aceptadas  */
+let nombre = 'Hola Mundo';
+let NombreCompleto;
+let nombreCompleto; /* esta (camelCase) es la mas aceptada y utilizada. */
+let nombre_completo;
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = 'Schurmann';
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,18 @@
+/*
+Esto es un comentario
+de muchas 
+líneas
+*/
+
+
+/**
+ * Este es un comentario
+ * de muchas
+ * líneas también 
+ * pero con un asterisco
+ * por cada
+ * enter
+ */
+let numero = 42;
+
+console.log(numero);
