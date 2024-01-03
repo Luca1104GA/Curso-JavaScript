e1e0926424c7edd1cbff3f46845ff807c4fa7a6b
Luca <almadaluca1104@gmail.com>
2024-01-03 17:30:20 -0300

    2hs de video

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,14 @@
+// Relacionaes
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+// Igualdad
+// Mal escrito
+console.log(a == 10);
+console.log(a != 10);
+// Bien escrito
+console.log(a === '10');
+console.log(a !== '10');
